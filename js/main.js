e5d2f6d2588014f8c1435cbe4d91ea338010b73f
hamza <hamzazaouya18@gmail.com>
2020-08-30 15:51:14 -0700

    Circles Below ?

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var slider = document.getElementById('demo');
+
+function slidImageOne() {
+    
+    slider.style.left = "0";
+    slider.classList.add('anim');
+    
+}
+
+function slidImageTwo() {
+    
+    slider.style.left = "-100%";
+    slider.classList.add('anim');
+    
+}
+
+function slidImageThree() {
+    
+    slider.style.left = "-200%";
+    slider.classList.add('anim');
+    
+}
+
+function slidImageFour() {
+    
+    slider.style.left = "-300%";
+    slider.classList.add('anim');
+    
+}
+
+function slidImageFive() {
+    
+    slider.style.left = "-400%";
+    slider.classList.add('anim');
+    
+}
+
+function slidImageSix() {
+    
+    slider.style.left = "-500%";
+    slider.classList.add('anim');
+    
+}
